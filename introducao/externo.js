370be3b3f9926ff68864ba373ec92c20c3683a5c
sandroaf <sandroaf@unidavi.edu.br>
2023-07-04 15:45:27 -0300

    Exemplos JavaScript DEVs-TI

---
+++ introducao/externo.js
@@ -0,0 +1,4 @@
+window.alert("Esse código é um JS do arquivo Externo");
+document.write("<h3>Essa linha veio do arquivo externo.js</h3>");
+let valor = 30; //declaração de variável
+document.write("O cubo de 30 é: "+(valor * valor * valor)); //operação aritimética 
